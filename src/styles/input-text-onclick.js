8afce6d5c825a76a4d57139cb6b678dc4ca50cfb
joyce.z <joyce.z@my-cpg.com>
2018-09-21 18:33:44 +0800

    2018/09/21 add input text onclick animation

---
+++ src/styles/input-text-onclick.js
@@ -0,0 +1,14 @@
+function callstart(){
+    var text=document.getElementById("call-move-up");
+    text.classList.add("move-up");
+}
+function phonestart(){
+    var text = document.getElementById("phone-move-up");
+    text.classList.add("move-up");
+}
+function qqstart(){
+    var text = document.getElementById("qq-move-up");
+    text.classList.add("move-up");
+}
+
+
