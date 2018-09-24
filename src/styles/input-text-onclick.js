d34fd5235e264f3b668a4168e1062e4fd21513c5
joyce.z <joyce.z@my-cpg.com>
2018-09-24 20:45:23 +0800

    2018/09/24 update input text onclick animation

--- src/styles/input-text-onclick.js
+++ src/styles/input-text-onclick.js
@@ -1,14 +1,44 @@
 function callstart(){
-    var text=document.getElementById("call-move-up");
-    text.classList.add("move-up");
+var text=document.getElementsByClassName("form-text-container-label")[0];
+text.classList.add("move-up");
 }
 function phonestart(){
-    var text = document.getElementById("phone-move-up");
-    text.classList.add("move-up");
+var text=document.getElementsByClassName("form-text-container-label")[1];
+text.classList.add("move-up");
 }
 function qqstart(){
-    var text = document.getElementById("qq-move-up");
+    var text=document.getElementsByClassName("form-text-container-label")[2];
     text.classList.add("move-up");
 }
+
+
+
+
+function callonblur(){
+    document.getElementById("call").placeholder = "稱呼";
+    document.getElementById("phone").placeholder = "联系电话";
+    document.getElementById("qq").placeholder = "联系QQ(Email必填";
+    document.getElementsByClassName("form-text-container-label")[0].classList.remove("move-up");
+    document.getElementsByClassName("form-text-container-label")[1].classList.remove("move-up");
+    document.getElementsByClassName("form-text-container-label")[2].classList.remove("move-up");
+    
+}
+//function phonestart(){
+//var text = document.getElementById("phone-move-up");
+//text.classList.add("move-up");
+//}
+//function qqstart(){
+//    var text = document.getElementById("qq-move-up");
+//    text.classList.add("move-up");
+//}
+
+
+
+    //document.getElementById("call").onclick = 
+    //function(){
+    //    var text = document.getElementsByClassName("form-text-container-lael");
+    //    alert("text");
+    //}
+
 
 
