2bf77a0e9cd67c591604e1f2accdabc20161a50e
joyce.z <joyce.z@my-cpg.com>
2018-09-28 15:15:41 +0800

    2018/09/28 add input text(call&qq) and radio validate (included css and jquery) / debug input display and button text position

--- src/js/form-validate.js
+++ src/js/form-validate.js
@@ -1,23 +1,90 @@
-$(function(){
-    var $call = $('#call');
+$(function () {
+    var $call = $('#call'),
+        $qq = $('#qq');
 
-    $call.blur(function(){
-        if ($(this).val() == ''){
-            $(this).parent().addClass('error');
-        } else{
-            if ($(this).parent().hasClass('error')){ //包含
-                $(this).parent().removeClass('error');
-            }
+
+        //判斷稱呼和聯繫QQ 是否填寫
+    $('#button').click(function (e) {
+  
+        if ($call.val() == '') {
+            $call.parent().addClass('error');
         }
+        if ($qq.val() == '') {
+            $qq.parent().addClass('error');
+            
+        }
+        if ($('.error').length > 0) {
+          e.preventDefault();
+          
+        }
+
     });
-});
+    //判斷radio 是否被選取
+    $("#button").click(function(){ 
+      var val=$('input:radio[name="lottery"]:checked').val();      if(val==null){
+          $(".form-radio-required").css("visibility","visible");               
+          return false;
+      }            
+        else{
+          //alert("有選到");
+          //alert(val); 
+          return true;
+          
+        }            
+        /*var list= $('input:radio[name="list"]:checked').val();     if(list==null){
+          alert("请选中一个!");
+          return false;
+        }else{ 
+          alert(list);}*/
+      }); 
 
 
-$('button[type="submit"]').click(function (e) {
-    if($call.val() == ''){
-        $call.parent().addClass('error');
-    }
-    if ($('error').lengh > 0){
-        e.preventDefault();
-    }
-});
+    /*$('#button').click(function (e) {
+      var select = $('input:radio[name="lottery"]:checked').val();
+  
+      if(select==null){
+          //alert("please selct one!!");
+          select.parent().addClass('error');
+          //return false;
+      }
+      else{
+          e.preventDefault();
+      }
+  
+    });*/
+            
+    /*$name.blur(function () {
+          if ($(this).val() == '') {
+              $(this).parent().addClass('error');
+          } else {
+              if ($(this).parent().hasClass('error')) {
+                  $(this).parent().removeClass('error');
+              }
+          }
+      });*/
+      /*$email.blur(function () {
+          var email = $(this).val(),
+              re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/igm;
+          if (re.test(email)) {
+              if ($(this).parent().hasClass('error')) {
+                  $(this).parent().removeClass('error');
+              }
+          } else {
+              $(this).parent().addClass('error');
+              $(this).parent().find('span').text('Not a valid email address');
+          }
+      });
+  
+      $message.blur(function () {
+          if ($(this).val() == '') {
+              $(this).parent().addClass('error');
+          } else {
+              if ($(this).parent().hasClass('error')) {
+                  $(this).parent().removeClass('error');
+              }
+          }
+      });*/
+    
+  });
+
+  
