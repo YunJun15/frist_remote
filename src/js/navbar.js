4cad7a0bd40822994bd6ee26912217da9652a915
joyce.z <joyce.z@my-cpg.com>
2018-10-03 15:09:12 +0800

    modifty navbar.js default is hidden

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,7 +1,14 @@
-$(document).ready(function(){
-    $('.nav-button').click(function(){
-        //$('.left').hide();
- $('.left').animate({width:'toggle'},550);
+$(document).ready(function () {
+    $('.nav-button').click(function () {
+        $('.navbar-container').css("font-size","14px");
+        $('.left').css("visibility", "visible");
+        $('.left').animate({ "width": "300px" });
     });
+
+    $('.nav-button').blur(function () {
+        $('.navbar-container').css("font-size","0px");
+        $('.left').animate({ "width": "0px" });
+    });
+
 });
 
