f748ab7611e372b268498a0aad28734a94fa742f
joyce.z <joyce.z@my-cpg.com>
2018-10-09 11:53:22 +0800

    modifty navbar undisplay

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,14 +1,20 @@
 $(document).ready(function () {
     $('.nav-button').click(function () {
-        $('.navbar-container').css("font-size","14px");
+        $('.navbar-container').css("visibility","visible");
         $('.left').css("visibility", "visible");
         $('.left').animate({ "width": "300px" });
     });
 
     $('.nav-button').blur(function () {
-        $('.navbar-container').css("font-size","0px");
+        $('.navbar-container').css("visibility","hidden");
+       // $('.active').preventDefault();
         $('.left').animate({ "width": "0px" });
+        //$('.left').animate({ "fontSize": "0px" });
+        
+
     });
+   
+
 
 });
 
